a0300f80a458ef36a62fcda40916591542bcf14b
juliacdss <juliacostaifc@gmail.com>
2026-03-12 11:07:40 -0300

    feat: criação da entidade para Livro

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -4,3 +4,4 @@ from .user import UserSerializer
 from .categoria import CategoriaSerializer
 from .editora import EditoraSerializer
 from .autor import AutorSerializer
+from .livro import LivroSerializer
--- core/views/__init__.py
+++ core/views/__init__.py
@@ -4,3 +4,4 @@ from .user import UserViewSet
 from .categoria import CategoriaViewSet
 from .editora import EditoraViewSet
 from .autor import AutorViewSet 
+from .livro import LivroViewSet
